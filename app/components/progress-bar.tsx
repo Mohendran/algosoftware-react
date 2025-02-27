2cebc7a08203accb614612e701503c003cca9cda
Mohendran <sparkmeout007@gmail.com>
2025-02-27 22:01:32 +0530

    UI Updated

--- app/components/progress-bar.tsx
+++ app/components/progress-bar.tsx
@@ -1,16 +1,18 @@
 export interface ProgressBarProps {
+  index: number;
   value: number;
   color: string;
 }
 
-export const ProgressBar = ({value, color}: ProgressBarProps) => {
+export const ProgressBar = ({index, value, color}: ProgressBarProps) => {
   return (
     <div>
       <div className="flex items-center justify-between gap-4">
-        <div className="text-sm text-gray-700 font-bold w-12">{value}%</div>
+        <div className="text-sm text-gray-700 font-bold w-12">Bar {index + 1}</div>
         <div className="w-full bg-gray-200 h-4 rounded-full">
           <div className={`h-full rounded-full ${color}`} style={{ width: `${value}%` }}></div>
         </div>
+        <div className="text-sm text-gray-700 font-bold w-12">{value}%</div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -66,62 +66,67 @@ export default function Home() {
           <input className="shadow appearance-none border rounded-sm w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="progressBarCount" type="number" min={1} max={100} placeholder="1-100" value={progressBarCount} onChange={(e) => updateProgressCount(e)}  />
         </div>
         <div className="flex items-end flex-1">
-          <button className="bg-blue-500 hover:bg-blue-700 hover:cursor-pointer text-white font-bold py-2 px-4 rounded-xl mt-4" onClick={() => generateProgressBars(progressBarCount)}>
+          <button className="bg-orange-400 hover:bg-orange-500 hover:cursor-pointer text-white font-bold py-2 px-4 rounded-xl mt-4" onClick={() => generateProgressBars(progressBarCount)}>
             Generate
           </button>
         </div>
       </div>
 
+      <div className="flex gap-8 w-2/3">
 
-
-      <h3 className="text-lg font-bold mt-8 mb-4">Update Progress</h3>
-      <div className="mb-4 flex gap-8">
-        <form className="w-2/3 flex gap-8" onSubmit={(e) => applyProgress(e)}>
-          <div className="flex-1">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="progressBarCount">
-              Select Progress Bar to Update:
-            </label>
-            <input className="shadow appearance-none border rounded-sm w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="progressBarCount" type="number" min={1} max={progressBarCount} placeholder="Progress Bar Index" value={progressBarToUpdate} onChange={(e) => setProgressBarToUpdate(parseInt((e.target as HTMLInputElement).value || '1', 10))} />
+        <div className="flex flex-1 flex-col">
+          <h3 className="text-lg font-bold mt-8 mb-4">Progress Bars</h3>
+          <div className="flex flex-col gap-4 max-w-[400px]">
+            {progressBarData.map((progressBar, index) => (
+              <ProgressBar index={index} key={index} value={progressBar.value} color={progressBar.color} />
+            ))}
           </div>
-          <div className="flex-1">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="progressBarCount">
-              Choose Color:
-            </label>
-            <div className=" flex gap-4 items-center">
-              <div className="flex gap-2 bg-red-500 p-2 pr-3 rounded-md">
-                <input type="radio" id="red" name="color" value="red" defaultChecked={progressBarColor === 'red'} onClick={() => setProgressBarColor('red')}/>
-                <label htmlFor="red" className="text-gray-700 text-sm font-bold text-white">Red</label>
+        </div>
+
+        <div className="flex flex-1 flex-col border-l-1 border-gray-300 pl-8">
+          <h3 className="text-lg font-bold mt-8 mb-4">Update Progress</h3>
+          <div className="mb-4 flex gap-8">
+            <form className="flex flex-col gap-4 max-w-[400px]" onSubmit={(e) => applyProgress(e)}>
+              <div className="flex-1">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="progressBarCount">
+                  Select Progress Bar to Update:
+                </label>
+                <input className="shadow appearance-none border rounded-sm w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="progressBarCount" type="number" min={1} max={progressBarCount} placeholder="Progress Bar Index" value={progressBarToUpdate} onChange={(e) => setProgressBarToUpdate(parseInt((e.target as HTMLInputElement).value || '1', 10))} />
               </div>
-              <div className="flex gap-2 bg-green-500 p-2 pr-3 rounded-md">
-                <input type="radio" id="green" name="color" value="green" defaultChecked={progressBarColor === 'green'} onClick={() => setProgressBarColor('green')}/>
-                <label htmlFor="green" className="text-gray-700 text-sm font-bold text-white">Green</label>
+              <div className="flex-1">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="progressBarCount">
+                  Choose Color:
+                </label>
+                <div className=" flex gap-4 items-center">
+                  <label htmlFor="red" className="text-sm font-bold text-white flex gap-1 bg-red-500 p-1 px-2 rounded-md cursor-pointer" onClick={() => setProgressBarColor('red')}>
+                    <input type="radio" id="red" name="color" value="red" defaultChecked={progressBarColor === 'red'}/>
+                    Red
+                  </label>
+                   <label htmlFor="green" className="text-sm font-bold text-white flex gap-1 bg-green-500 p-1 px-2 rounded-md cursor-pointer" onClick={() => setProgressBarColor('green')}>
+                    <input type="radio" id="green" name="color" value="green" defaultChecked={progressBarColor === 'green'}/>
+                    Green
+                  </label>
+                  <label htmlFor="blue" className="text-sm font-bold text-white flex gap-1 bg-blue-500 p-1 px-2 rounded-md cursor-pointer" onClick={() => setProgressBarColor('blue')}>
+                    <input type="radio" id="blue" name="color" value="blue" defaultChecked={progressBarColor === 'blue'}/>
+                    Blue
+                  </label>
+                </div>
               </div>
-              <div className="flex gap-2 bg-blue-500 p-2 pr-3 rounded-md">
-                <input type="radio" id="blue" name="color" value="blue" defaultChecked={progressBarColor === 'blue'} onClick={() => setProgressBarColor('blue')}/>
-                <label htmlFor="blue" className="text-gray-700 text-sm font-bold text-white">Blue</label>
+              <div className="flex-1">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="ProgressBarValue">
+                  Progress Bar Value:
+                </label>
+                <input className="shadow appearance-none border rounded-sm w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="ProgressBarValue" type="number" min={0} max={100} placeholder="Progress Bar Value" value={progressBarValue} onChange={(e) => setProgressBarValue(parseInt((e.target as HTMLInputElement).value || '0', 10))} />
               </div>
-            </div>
+              <div className="flex justify-end items-end">
+                <button type="submit" className="bg-orange-400 hover:bg-orange-500 hover:cursor-pointer text-white font-bold py-2 px-4 rounded-xl mt-4">
+                  Make Progress
+                </button>
+              </div>
+            </form>
           </div>
-          <div className="flex-1">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="ProgressBarValue">
-              Progress Bar Value:
-            </label>
-            <input className="shadow appearance-none border rounded-sm w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="ProgressBarValue" type="number" min={0} max={100} placeholder="Progress Bar Value" value={progressBarValue} onChange={(e) => setProgressBarValue(parseInt((e.target as HTMLInputElement).value || '0', 10))} />
-          </div>
-          <div className="flex items-end">
-            <button type="submit" className="bg-blue-500 hover:bg-blue-700 hover:cursor-pointer text-white font-bold py-2 px-4 rounded-xl mt-4">
-              Make Progress
-            </button>
-          </div>
-        </form>
-      </div>
+        </div>
 
-
-      <h3 className="text-lg font-bold mt-8 mb-4">Progress Bars</h3>
-      <div className="flex flex-col gap-4 max-w-[400px]">
-        {progressBarData.map((progressBar, index) => (
-          <ProgressBar key={index} value={progressBar.value} color={progressBar.color} />
-        ))}
       </div>
 
     </div>
